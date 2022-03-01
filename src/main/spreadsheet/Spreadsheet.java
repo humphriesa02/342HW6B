48932341bbc29f003ae353ed0f1dce3eb97e0f3b
Alex Humphries <humphriesa02@gmail.com>
2022-02-28 16:45:31 -0800

    Added Spreadsheet.java class.

---
+++ src/main/spreadsheet/Spreadsheet.java
@@ -0,0 +1,13 @@
+package main.spreadsheet;
+
+public class Spreadsheet {
+    private static final int rows = 4;
+    private static final int columns = 4;
+
+    public Spreadsheet(){
+
+    }
+    public Spreadsheet(int rowCols){
+
+    }
+}
