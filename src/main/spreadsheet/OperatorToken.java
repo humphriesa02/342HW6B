209692207fce8e7c4bc527ba281c90fa3a641f63
dylanLf47 <dylanlucasfrederick@gmail.com>
2022-02-28 16:11:46 -0800

    Added OperatorToken class

---
+++ src/main/spreadsheet/OperatorToken.java
@@ -0,0 +1,11 @@
+package main.spreadsheet;
+
+public class OperatorToken extends Token {
+    public static final char Plus  = '+';
+    public static final char Minus = '-';
+    public static final char Mult  = '*';
+    public static final char Div   = '/';
+    public static final char LeftParen  = '(';
+
+    private char operatorToken;
+}
