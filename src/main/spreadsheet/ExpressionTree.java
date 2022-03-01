baa9f88089e22518643020de7f6aefb3d1460328
dylanLf47 <dylanlucasfrederick@gmail.com>
2022-02-28 16:32:19 -0800

    Added ExpressionTree class

---
+++ src/main/spreadsheet/ExpressionTree.java
@@ -0,0 +1,9 @@
+package main.spreadsheet;
+
+public class ExpressionTree {
+    private ExpressionTreeNode root;
+
+    public void makeEmpty();
+    public void printTree();
+    public int Evaluate(Spreadsheet spreadsheet);
+}
