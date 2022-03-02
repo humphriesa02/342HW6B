0b76baba4357e86c445d948e9d3650655c5b225f
dylanLf47 <dylanlucasfrederick@gmail.com>
2022-03-02 13:26:25 -0800

    Added getColumn and getCell for CellToken

--- src/main/spreadsheet/CellToken.java
+++ src/main/spreadsheet/CellToken.java
@@ -3,4 +3,12 @@ package main.spreadsheet;
 public class CellToken extends Token {
     private int column; // column A = 0, B = 1, etc.
     private int row;
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
 }
