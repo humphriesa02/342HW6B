04139fbf254e19e8a682b569e3a81ef2f50d297d
alveeno <nguyenalvind@gmail.com>
2022-03-04 12:32:30 -0800

    Added parts of tree creation.

--- src/main/spreadsheet/ExpressionTree.java
+++ src/main/spreadsheet/ExpressionTree.java
@@ -1,17 +1,54 @@
 package main.spreadsheet;
 
+import java.util.Stack;
+
 public class ExpressionTree {
+
     private ExpressionTreeNode root;
 
-    public void makeEmpty(){
+    // Initialize an Empty tree.
+    public ExpressionTree() {
+        this.root = null;
+    }
 
+    // Initialize ExpressionTree with root.
+    public ExpressionTree(final ExpressionTreeNode myRoot) {
+        this.root = myRoot;
+    }
+
+    // Empty tree.
+    public void makeEmpty(){
+        this.root = null;
+    }
+
+    /**
+     * Makes a tree from stack of tokens.
+     */
+    public void tokenTree(Stack<Token> stackOfTokens) {
+        root = getTokens(stackOfTokens);
+        if (!stackOfTokens.isEmpty()) {
+            System.out.println("Cannot make a tree!");
+        }
+    }
+
+    /**
+     * Organize tokens from stack into a tree. (STILL WORKING ON METHOD)
+     */
+    public ExpressionTreeNode getTokens(Stack<Token> stackOfTokens) {
+        ExpressionTreeNode tree = null;
+        Token token;
+
+        if(stackOfTokens.isEmpty())
+            return null;
+
+        return null; // ignore for now.
     }
     public void printTree(){
 
     }
-    public int Evaluate(Spreadsheet spreadsheet){
-
-    }
+//    public int Evaluate(Spreadsheet spreadsheet){
+//
+//    }
 
     ExpressionTreeNode GetExpressionTree(Stack s) {
         ExpressionTreeNode returnTree;
